1c553527743ff483f7307b0d5b24440992009f51
Cisne Parra <adparra2@utpl.edu.ec>
2023-11-23 15:59:40 -0500

    cambios de la clase

--- EjemplosDoWhile/Ejemplos/src/ejemplos02/Ejemplo02.java
+++ EjemplosDoWhile/Ejemplos/src/ejemplos02/Ejemplo02.java
@@ -31,11 +31,13 @@ public class Ejemplo02 {
             cadenaFinal = String.format("%s%.2f\n", cadenaFinal, nota);
             entrada.nextLine(); // limpieza de buffer
 
-            System.out.println("Ingrese (s) si desea salir del ciclo ");
+            System.out.println("Ingrese (si, yes, s o y) si desea salir del ciclo ");
             salida = entrada.nextLine();
 
-            
-            
+            if (salida.equals("s") || salida.equals ("yes")
+                    || salida.equals("si") || salida.equals ("y")) { // f
+                bandera = false;
+            }
 
         } while (bandera); // (bandera==true)
 
